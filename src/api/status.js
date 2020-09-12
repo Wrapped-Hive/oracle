fb798dccf5442ab9eac48abda296bea630aefeac
fbslo <admin@fbslo.net>
2020-09-12 15:17:57 +0200

    fix

--- src/api/status.js
+++ src/api/status.js
@@ -18,7 +18,7 @@ router.get("/", async (req, res) => {
 
 function getFee(){
   return new Promise((resolve, reject) => {
-    database.findOne({type: "fee"}, (err, result) => {
+    database.findOne({type: "leo_fee"}, (err, result) => {
       if (err) reject(err)
       else resolve(result.fee)
       })
